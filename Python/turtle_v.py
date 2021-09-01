f7b98b8b7b6dd42b76bf7e1e307383ba24d81cd4
BinayakBishnu <bishnu.binayak12@gmail.com>
2021-09-01 10:45:22 +0530

    styx+travelmigo

--- Python/turtle_v.py
+++ Python/turtle_v.py
@@ -1,12 +1,30 @@
 from turtle import *
 speed(80)
-color('blue')
+color('red')
 bgcolor('black')
 b = 200
 while b>0:
     left(b)
     forward(b*3)
     b = b-1
+
+# speed(80)
+# color('blue')
+# bgcolor('black')
+# b = 200
+# while b>0:
+#     left(b)
+#     forward(b*3)
+#     b = b-1
+
+# speed(80)
+# color('purple')
+# bgcolor('black')
+# b = 200
+# while b>0:
+#     left(b)
+#     forward(b*3)
+#     b = b-1
 
 Screen().exitonclick()
 
