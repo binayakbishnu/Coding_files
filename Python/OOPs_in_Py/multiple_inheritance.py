a916bb5940a240f50bae37befab232a62a23b298
BinayakB <bishnu.binayak12@gmail.com>
2022-01-24 13:31:39 +0530

    multiple inheritance oops in py

--- Python/OOPs_in_Py/multiple_inheritance.py
+++ Python/OOPs_in_Py/multiple_inheritance.py
@@ -1,4 +1,4 @@
-class Student:
+class Fresher:
     def __init__(self, name, yearofadmission, roll):
         self.name = name
         self.yearofadmission = yearofadmission
@@ -6,18 +6,38 @@ class Student:
 
     def application_no(self):
         name_broken = self.name.split(' ')
-        initial = name_broken[0][0] + name_broken[1][0]
-        applno = initial + self.roll
-        return applno
+        self.applno = name_broken[0][0] + name_broken[1][0] + self.roll
+        return self.applno
+
+    def name_print_in_child(self):
+        print(self.name)
+
 
 class Core:
-    def __init__(self,domain):
+    def __init__(self, domain):
         self.domain = domain
 
-class Fresher_Core(Student, Core):
-    def __init_(self,)
+
+class Fresher_Core(Fresher, Core):
+    def __init__(self, name, yearofadmission, roll, domain):
+        Fresher.__init__(self, name, yearofadmission, roll)
+        Core.__init__(self, domain)
+
+        Fresher.name_print_in_child(self)
 
 
-student1 = Student('Binayak Bishnu', 2020, '20BIT0155')
-print(student1.__dict__)
-print(student1.application_no())
+fresher1 = Fresher('Binayak Bishnu', 2020, '20BIT0155')
+print(fresher1.__dict__)
+# print(fresher1.applno)    # error
+print(fresher1.application_no())
+print(fresher1.applno)
+print(fresher1.__dict__)
+
+print();print()
+
+fresher2 = Fresher_Core('Neon Neo', 2021, '21BIT0000', 'Webdev')
+print(fresher2.__dict__)
+print(fresher2.yearofadmission)
+print(fresher2.application_no())
+print(fresher2.applno)
+print(fresher2.__dict__)
